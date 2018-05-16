5c1953936b1b33c374e61e0b1380366d3bf7791c
Bode, Michael <michael.bode@aruplab.com>
2018-05-16 13:49:39 -0600

    Reading in variables.

--- vars/logger.groovy
+++ vars/logger.groovy
@@ -1,4 +1,11 @@
 def call(global_level, level, message) {
+	def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+	echo config.WorkingJob
+	echo config.workingProject
+	
 	def emit = false
 
 	//Compare global logging level to message level, emit if message level less than global
