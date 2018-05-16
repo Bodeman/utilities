59c3e90124b1192a4ae82175698b4f8a5e727c36
Bode, Michael <michael.bode@aruplab.com>
2018-05-16 13:34:38 -0600

    Expanded logic. Apparently switch case is not allowed.

--- vars/logger.groovy
+++ vars/logger.groovy
@@ -1,7 +1,11 @@
 def call(global_level, level, message) {
 	def emit = false
+
+	//Compare global logging level to message level, emit if message level less than global
 	if(global_level == "DEBUG") {emit = true} 
 	if(global_level == "INFO" && level != "DEBUG") {emit = true}
+	if(global_level == "WARN" && (level != "DEBUG" || level != "INFO") {emit = true}
+	if(global_level == "ERROR" && level == "ERROR") {emit = true}
 
 	if(emit) {echo "${level}: ${message}"}
 
