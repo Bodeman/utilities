6c1e03e245ee6c1d6a3e3eb001d5d5161f0cff18
Bode, Michael <michael.bode@aruplab.com>
2018-05-17 09:49:12 -0600

    Not sure what is going on.

--- vars/EnvVars.groovy
+++ vars/EnvVars.groovy
@@ -1,11 +1,11 @@
 def call(mode) {
 	try{
-	if(mode == 0) {
-		env.workingGitURL = "CAMMIS"
-	}
-	else {
-		env.workingGitURL = 'https://github.com/Bodeman/Dev.git'  
-	}
+		if(mode == 0) {
+			env.workingGitURL = "CAMMIS"
+		}
+		else {
+			env.workingGitURL = 'https://github.com/Bodeman/Dev.git'  
+		}
 	}
 	catch (e) {
 		echo "${e}"
