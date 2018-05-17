15ca15ce18939215eaadb70f143598e3710e5289
Bode, Michael <michael.bode@aruplab.com>
2018-05-17 10:40:27 -0600

    Expanding functions

--- vars/workingconfigs.groovy
+++ vars/workingconfigs.groovy
@@ -11,3 +11,42 @@ def seturl(mode) {
 		echo "${e}"
 	}
 }
+def setbranch(mode) {
+	try{
+		if(mode == 0) {
+			return 'errorTest'
+		}
+		else {
+			return 'errorTest'  
+		}
+	}
+	catch (e) {
+		echo "${e}"
+	}
+}
+def setPOM(mode) {
+	try{
+		if(mode == 0) {
+			return '/var/lib/jenkins/workspace/TestPipeline/SpringPOC'
+		}
+		else {
+			return '/var/lib/jenkins/workspace/TestPipeline/SpringPOC'  
+		}
+	}
+	catch (e) {
+		echo "${e}"
+	}
+}
+def setJenkinsDir(mode) {
+	try{
+		if(mode == 0) {
+			return '/var/lib/jenkins/workspace'
+		}
+		else {
+			return '/var/lib/jenkins/workspace/TestPipeline/SpringPOC'  
+		}
+	}
+	catch (e) {
+		echo "${e}"
+	}
+}
