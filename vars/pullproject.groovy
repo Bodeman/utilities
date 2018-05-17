4dc2a4bfdfda3bcc798533869166fb3889aaa91f
Bode, Michael <michael.bode@aruplab.com>
2018-05-17 08:47:50 -0600

    Try to simplify pullproject, probably won't work.

---
+++ vars/pullproject.groovy
@@ -0,0 +1,11 @@
+def call(workingGitURL, workingBranch) {
+	try {
+		logger "${loglevel}", "DEBUG", "Attempting git pull ${workingGitURL}, branch: ${workingBranch}"
+		git url: "${workingGitURL}", branch: "${workingBranch}"
+		}
+	catch (e) {
+		logger "${loglevel}", "ERROR", "Pull failed. Error[${e}]"
+		continueBuild = false
+		throw
+		}
+}
