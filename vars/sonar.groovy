1a05b326d6bf5b43778053525b6c711ed7f4c46c
Bode, Michael <michael.bode@aruplab.com>
2018-05-29 14:07:30 -0600

    adding sonar function

---
+++ vars/sonar.groovy
@@ -0,0 +1,27 @@
+def call(mode, SonarHost) {
+	try {
+		if(mode == 'Windows') {
+			bat 'mavn sonar:sonar' +
+					' -Dsonar.host.url=${SonarHost}' + 
+					' -Dsonar.projectVersion=1.0' +
+					' -Dsonar.sourceEncoding=UTF-8' +
+					' -Dsonar.projectKey="${workingJob}"' +
+					' -Dsonar.java.binaries="${workingJenkinsDir}"/"${workingJob}"/"${workingProject}"/target/classes' +
+					' -Dsonar.sources="${workingProject}"/src' +
+					' -Dsonar.projectBaseDir="${workingJenkinsDir}"/"${workingJob}"'		
+			}
+		else {
+			sh '/var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/Sonar/bin/sonar-scanner' +
+					' -Dsonar.host.url=${SonarHost}' + 
+					' -Dsonar.projectVersion=1.0' +
+					' -Dsonar.sourceEncoding=UTF-8' +
+					' -Dsonar.projectKey="${workingJob}"' +
+					' -Dsonar.java.binaries="${workingJenkinsDir}"/"${workingJob}"/"${workingProject}"/target/classes' +
+					' -Dsonar.sources="${workingProject}"/src' +
+					' -Dsonar.projectBaseDir="${workingJenkinsDir}"/"${workingJob}"'
+			}
+		}
+	catch (e) {
+		logger "${loglevel}", "ERROR", "Pull failed. Error[${e}]"
+		}
+}
