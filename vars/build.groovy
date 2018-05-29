2b344389efb0c5f923ed89bd27d5cc8d7a3d4044
Bode, Michael <michael.bode@aruplab.com>
2018-05-29 07:54:13 -0600

    Added build disable

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,9 +1,11 @@
-def call(mode, mvnHome, POMLocation) {
+def call(mode, mvnHome, POMLocation, disable) {
 	try {
-		if(mode == "Windows") {
-			bat "mvn -X -B --file ${POMLocation} -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
-		} else {
-			sh "'${mvnHome}/bin/mvn' -X -B --file '${POMLocation}' -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
+		if(disable != "TRUE") {
+			if(mode == "Windows") {
+				bat "mvn -X -B --file ${POMLocation} -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
+			} else {
+				sh "'${mvnHome}/bin/mvn' -X -B --file '${POMLocation}' -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
+			}
 		}
 	}
 	catch (e) { echo "ERROR: ${e}"}
