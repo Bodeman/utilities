a63b58a1e3202b6b2decc73a6d0f704411cf7b50
Bode, Michael <michael.bode@aruplab.com>
2018-05-29 13:30:28 -0600

    Added loglevel to build function

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,10 +1,16 @@
 def call(mode, mvnHome, POMLocation, disable) {
+	if (loglevel == "DEBUG") {
+		add_debug = "-X"
+	}
+	if(loglevel == "WARNING" || loglevel == "ERROR") {
+		remove_info = "-q"
+	}
 	try {
 		if(disable != true) {
 			if(mode == "Windows") {
-				bat "mvn -X -B --file ${POMLocation} -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
+				bat "mvn ${add_debug} ${remove_info} -B --file ${POMLocation} -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
 			} else {
-				sh "'${mvnHome}/bin/mvn' -X -B --file '${POMLocation}' -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
+				sh "'${mvnHome}/bin/mvn' ${add_debug} ${remove_info} -B --file '${POMLocation}' -Dmaven.test.failure.ignore clean install cobertura:cobertura -Dcobertura.report.format=xml"
 			}
 		}
 		else {echo "Skipping compile: ${disable}"}
